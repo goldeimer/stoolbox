f3c6727a5744aa3cf745ff3c4977abb11b2bca1c
gottemeyer <frederik@gottemeyer.ms>
2022-12-15 16:09:01 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Working Title: PooPeeFY',
-  tagline: 'Poo & Pee for yourself',
+  title: 'sToolbox',
+  tagline: 'powered by Goldeimer',
   url: 'https://stoolbox.org',
   baseUrl: '/stoolbox/',
   onBrokenLinks: 'throw',
