c20d0bb437b548d14b3b05ac92dc3d151b2e0972
gottemeyer <frederik@gottemeyer.ms>
2023-02-14 17:16:15 +0100

    Update donation.js

--- src/pages/donation.js
+++ src/pages/donation.js
@@ -12,15 +12,6 @@ export default function Hello() {
           height: '50vh',
           fontSize: '20px',
         }}>
-        <script type="text/javascript">
-          (function() {
-            var u="https://spenden.twingle.de/embed/goldeimer/stoolbox/tw63eb739b2cebb/form";
-            var id = '_' + Math.random().toString(36).substr(2, 9);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            document.write('<div id="twingle-public-embed-' + id + '"></div>');
-            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'/'+id; s.parentNode.insertBefore(g,s);
-          })();
-        </script>
         <p>
           Edit <code>pages/helloReact.js</code> and save to reload.
         </p>
